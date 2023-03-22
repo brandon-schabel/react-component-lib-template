af0ee377f5d0d15af10f1f2fd939fd5d9fd61f27
brandon-schabel <brandonschabel1995@gmail.com>
2023-03-22 10:02:17 -0700

    add update build configuration and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 import tsConfigPaths from "vite-tsconfig-paths";
 import * as packageJson from "./package.json";
 
-export default defineConfig((configEnv) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
     tsConfigPaths(),
@@ -17,19 +17,17 @@ export default defineConfig((configEnv) => ({
     lib: {
       entry: resolve("src", "index.ts"),
       name: "react-component-library",
-      formats: ["es", "umd"],
-      fileName: (format) => `react-component-library.${format}.js`,
+      formats: ["es", "cjs"],
+      fileName: (format) =>
+        `react-component-library.${
+          format === "cjs" ? "cjs" : "es.js"
+        }`,
     },
-    rollupOptions: {
-      // externalize dependencies that shouldn't be bundled,
-      // e.g. react and react-dom should be peer dependencies
-      external: [...Object.keys(packageJson.peerDependencies)],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+    optimizeDeps: {
+      exclude: Object.keys(packageJson.peerDependencies),
+    },
+    esbuild: {
+      minify: true,
     },
   },
 }));
